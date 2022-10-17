ccc13055c8100f05a0614763b6a7622c42fbb0fb
CarllosMiranda <mirandac215@gmail.com>
2022-10-16 21:26:17 -0300

    express

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express')
-const {resolve, dirname} = require('path')
+
 const app = express()
 
-app.use('/', 
-express.static(
-    resolve(__dirname,'./build')
-    ))
+app.use(express.static('public'));
 
 
 app.listen(
