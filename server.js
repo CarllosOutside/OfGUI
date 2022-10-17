d72f6e6af9a393379472dc9625ae35d29dafd5db
CarllosMiranda <mirandac215@gmail.com>
2022-10-16 22:32:12 -0300

    te

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express')
-const path = require('path');
+const {resolve} = require('path');
 
 const app = express()
 
-app.use(express.static('../build'));
-
-app.get('*', (req, res)=> {
-  const index = path.join(__dirname, '/', '../build', 'index.html' );
-  res.sendFile(index);
-});
-
+app.use(express.static(
+    
+    resolve(__dirname, './build')
+    
+    ));
 
 app.listen(
     process.env.PORT || 3000, (err) =>{
