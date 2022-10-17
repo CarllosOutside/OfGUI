2c6b09cf4f5347ae9fb8397e7139b51d2026776c
CarllosMiranda <mirandac215@gmail.com>
2022-10-16 21:38:02 -0300

    getTest

--- server.js
+++ server.js
@@ -12,3 +12,15 @@ app.listen(
         console.log("Ook")
     }
 )
+
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            name: 'name of your app',
+            version: '0.1.0'
+        }
+    });
+
+});
