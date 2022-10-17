eba9ba15cc21a137c85ea5271906ce672502ede5
CarllosMiranda <mirandac215@gmail.com>
2022-10-16 22:12:36 -0300

    teste

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const app = express()
 
 app.use(express.static('build'));
 
-
+app.get('/teste', function(req, res) {
+    res.send("ola")
+    console.log("esta chamando")
+  });
 app.listen(
     process.env.PORT || 3000, (err) =>{
         if(err){return console.log(err)}
 
-        console.log("Ook")
+        console.log("Okey")
     }
 )
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-  });
