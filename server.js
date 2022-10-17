5aa34a8b191348ff59104d0bbecc02d5f431c68f
CarllosMiranda <mirandac215@gmail.com>
2022-10-16 22:42:09 -0300

    tell

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const {resolve} = require('path');
 
 const app = express()
-
+app.get('/', function(req, res) {
+    res.sendFile(resolve(__dirname, './build') );
+});
 app.use(express.static(
-    
-    resolve(__dirname, './build')
-    
+    resolve(__dirname, './build') 
     ));
 
 app.listen(
