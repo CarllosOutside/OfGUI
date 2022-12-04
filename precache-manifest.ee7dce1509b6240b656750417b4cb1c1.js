45a8c9603373f7516e7504c650a1fe0e3c1bca3f
CarllosMiranda <mirandac215@gmail.com>
2022-12-04 12:47:21 -0300

    Updates

---
+++ precache-manifest.ee7dce1509b6240b656750417b4cb1c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "453e8e49f565323f52ca",
+    "url": "/OfGUI/static/css/main.c7eb6521.chunk.css"
+  },
+  {
+    "revision": "453e8e49f565323f52ca",
+    "url": "/OfGUI/static/js/main.453e8e49.chunk.js"
+  },
+  {
+    "revision": "e9cf3b800fc772bf4983",
+    "url": "/OfGUI/static/js/1.e9cf3b80.chunk.js"
+  },
+  {
+    "revision": "36f46783649582888913",
+    "url": "/OfGUI/static/css/2.7fa64263.chunk.css"
+  },
+  {
+    "revision": "36f46783649582888913",
+    "url": "/OfGUI/static/js/2.36f46783.chunk.js"
+  },
+  {
+    "revision": "c03bb2d7f83e72b387c0",
+    "url": "/OfGUI/static/js/runtime~main.c03bb2d7.js"
+  },
+  {
+    "revision": "8b60710c91946ec1b9303e944a71a20f",
+    "url": "/OfGUI/index.html"
+  }
+];
