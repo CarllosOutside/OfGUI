9d0cc5d6e9556a6e6193ffa323c9a05ad7ed1d07
CarllosMiranda <mirandac215@gmail.com>
2022-12-04 12:08:27 -0300

    serv

--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "https://oficina-gesma.herokuapp.com/api",
-  //proxy: "https://oficina-gesma.herokuapp.com/",
-  baseURL: "http://localhost:8080/api",
-  proxy: "http://localhost/",
+  baseURL: "https://oficinamecanica-production.up.railway.app/api",
+  proxy: "https://oficinamecanica-production.up.railway.app/",
+  //baseURL: "http://localhost:8080/api",
+  //proxy: "http://localhost/",
   headers: {
     "Content-type": "application/json"
   }
